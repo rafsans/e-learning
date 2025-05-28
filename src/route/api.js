b0d8de3d692d1e6afa8b7261c5447abc6527a2aa
ivan <ivanchandra8d@gmail.com>
2025-05-28 14:54:09 +0700

    update user

--- src/route/api.js
+++ src/route/api.js
@@ -1,11 +1,14 @@
 import express from "express";
 import coursesController from "../controllers/coursesController.js";
+import usersController from "../controllers/usersController.js";
 
 const appRouter = express.Router();
 
 appRouter.get("/", (req, res) => {
   res.send("Salamat Thanks!ssss");
 });
+
 appRouter.get('/course', coursesController.getAllCourses)
+appRouter.get('/users', usersController.getAllUser);
 
 export default appRouter;
