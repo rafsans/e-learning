740f05300f89dc409d8c2801effa86f61dc19e19
Theosxx <muhammadhilal01tr@gmail.com>
2025-05-28 22:57:20 +0700

    Add Auth

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import express from "express";
 const app = express();
 const port = 300;
+import authRoute from './route/authRoute.js';
+
+app.use(express.json());
+app.use('/api/auth', authRoute);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
