2e4939f7ed2d5839a42bddd543934eb9f82c20ce
ivan <ivanchandra8d@gmail.com>
2025-05-28 17:06:05 +0700

    updaate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
 import App from "./app/app.js";
 
-<<<<<<< HEAD
-app.get("/", (req, res) => {
-  res.send("123!");
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
-=======
 const appInstance = new App();
 appInstance.start(); // penting!
->>>>>>> b0d8de3d692d1e6afa8b7261c5447abc6527a2aa
