399b6d08dc5cbceed8ad1f616fecd3b963e8fa95
Rafsan <Rafsanbro12@gmail.com>
2025-06-01 16:40:42 +0700

    fixing index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authRoute from './route/api.js';
+import routes from './route/api.js';
 import courseRoute from './route/api.js';
 import swaggerUi from 'swagger-ui-express';
 import swaggerSpec from './docs/swagger.js';
@@ -7,8 +7,7 @@ import swaggerSpec from './docs/swagger.js';
 const port = 3000;
 const app = express();
 app.use(express.json());
-app.use('/api/auth', authRoute);
-app.use('/api/', courseRoute);
+app.use('/api/', routes);
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.listen(port, () => {
