662e2670ed9bbc014657d5526afaece7c3409ebb
nurrr357 <muhamadhuda@gmail.com>
2025-06-01 20:25:12 +0700

    fixing course section

--- src/models/courses/courseModel.js
+++ src/models/courses/courseModel.js
@@ -1,15 +1,11 @@
 import { prisma } from "../../db/config.js";
 
 const courseModel = {
-    async getAllCourses() {
-        try {
-            return await prisma.courses.findMany();
-        } catch (error) {
-            console.error("Error fetching courses:", error);
-            throw error;
-        }
-    },
-
-}
+    getAllCourses: async () => await prisma.courses.findMany(),
+    getCourseById: async (id) => await prisma.courses.findUnique({ where: { id } }),
+    post: async (body) => await prisma.courses.create({ data: body }),
+    put: async (id, body) => await prisma.courses.update({ where: { id }, data: body }),
+    delete: async (id) => await prisma.courses.delete({ where: { id } }),
+};
 
 export default courseModel;
