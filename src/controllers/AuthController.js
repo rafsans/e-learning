632592233707080deb5d94ffd072ddf3000d7102
Rafsan <Rafsanbro12@gmail.com>
2025-06-05 14:25:28 +0700

    fixing auth controller

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,68 +1,53 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+import authModel from '../models/authModel.js';
+import { registerSchema, loginSchema } from '../validation/authValidation.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const AuthController = {
-  // Register
   register: async (req, res) => {
     try {
-      const { email, password } = req.body;
-
-      if (!email || !password) {
-        return res.status(400).json({ message: 'wajib' });
+      const body = req.body;
+      const { error } = registerSchema.validate(body, { abortEarly: false });
+      if (error) {
+        const validationError = error.details.map((err) => ({
+          field: err.path[0],
+          message: err.message.replace(/"/g, ''),
+        }));
+        return res.status(422).json({ status: false, message: 'Validation Error', errors: validationError });
       }
-      const existingUser = await prisma.user.findUnique({ where: { email } });
-      if (existingUser) {
-        return res.status(409).json({ message: 'sudah ada' });
-      }
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-
-      const newUser = await prisma.user.create({
-        data: {
-          email,
-          password: hashedPassword
-        }
-      });
-
-      res.status(201).json({ message: 'Dadi COk', user: { id: newUser.id, email: newUser.email } });
+      await authModel.register(body);
+      return res.status(201).json({ status: true, message: 'Success' });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Gagal Dancok ' });
+      return res.status(500).json({ status: false, message: 'Server Error' });
     }
   },
 
   login: async (req, res) => {
     try {
-      const { email, password } = req.body;
+      const body = req.body;
+      const { error } = loginSchema.validate(body, { abortEarly: false });
+      if (error) {
+        const validationError = error.details.map((err) => ({
+          field: err.path[0],
+          message: err.message.replace(/"/g, ''),
+        }));
+        return res.status(422).json({ status: false, message: 'Validation Error', errors: validationError });
+      }
 
-      const user = await prisma.user.findUnique({ where: { email } });
-
+      const user = await authModel.login(body.email);
       if (!user) {
-        return res.status(404).json({ message: 'Email tidak ada' });
+        return res.status(401).json({ status: false, message: 'Unauthorized' });
       }
-
-      const passwordMatch = await bcrypt.compare(password, user.password);
-      if (!passwordMatch) {
-        return res.status(401).json({ message: 'Salah' });
-      }
-
-      const token = jwt.sign(
-        { id: user.id, email: user.email },
-        process.env.JWT_SECRET,
-        { expiresIn: '1d' }
-      );
-
-      res.status(200).json({
-        message: 'Berhasil',
-        token
-      });
+      const token = jwt.sign({ id: user.id }, process.env.SECRET_KEY);
+      return res.status(200).json({ status: true, message: 'Success', data:{
+        name: user.name,
+        email: user.email,
+        token: token
+      } });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Salah' });
+      return res.status(500).json({ status: false, message: error.message });
     }
   }
 
