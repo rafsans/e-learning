bffcacdec558ede6f4455b84024430301e9536a5
Rafsan <Rafsanbro12@gmail.com>
2025-06-09 19:18:37 +0700

    update config swagger

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -1,22 +1,13 @@
 import swaggerJSDoc from 'swagger-jsdoc';
-
-const options = {
+const swaggerSpec = swaggerJSDoc({
   definition: {
     openapi: '3.0.0',
     info: {
-      title: 'E-Learning API',
+      title: 'E - Learning API',
       version: '1.0.0',
-      description: 'API dokumentasi untuk sistem E-Learning',
+      description: 'E - Learning API',
     },
-    servers: [
-      {
-        url: 'http://localhost:3000/api',
-        description: 'Local server',
-      },
-    ],
   },
-  apis: ['./src/docs/*.yaml'], // nanti kita buat file YAML di sini
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+  apis: ['../docs/api.yaml'],
+});
 export default swaggerSpec;
